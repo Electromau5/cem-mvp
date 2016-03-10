47cceb4771e0a110ad2e7dc9c0f3da0d5210ec68
Pritish Sai <Pritish.sai@gmail.com>
2016-03-10 22:17:35 +0530

    added sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 	
+	before_action :set_users, only: [:edit, :update, :show, :destroy]
+	#before_action :require_same_user, only: [:edit, :update, :show, :destroy]
+	before_action :require_admin, only: [:edit, :update, :show, :destroy]
+	
 	def index
 		@user = User.all
 	end
@@ -11,6 +15,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 			if @user.save
+				session[:user_id] = @user.id
 				#flash[:success] = "Welcome to the alpha blog #{@user.username}"
 				redirect_to product1s_path	
 			else
@@ -19,11 +24,9 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
 			if @user.update(user_params)
 				#flash[:success] = "Your account was updated successfully"
 				redirect_to product1s_path
@@ -33,7 +36,6 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
 	end
 
 	def destroy
@@ -44,5 +46,16 @@ class UsersController < ApplicationController
 private
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
+	end
+
+	def set_users
+		@user = User.find(params[:id])
+	end
+
+	def require_same_user
+		if logged_in? && current_user != @user
+			#flash[:danger] = "You can only edit your own account"
+			redirect_to root_path
+		end
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309211610) do
+ActiveRecord::Schema.define(version: 20160310163004) do
 
   create_table "product1s", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160309211610) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
